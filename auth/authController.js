20b5a242f8e2a959d5f28d58447341f2a88278ca
Pavelkv94 <pavel.kv94@yandex.by>
2022-08-12 18:15:03 +0300

    add swagger docs

--- auth/authController.js
+++ auth/authController.js
@@ -4,107 +4,99 @@ const bcrypt = require("bcryptjs"); //хэширование пароля
 const { validationResult } = require("express-validator"); //для получения сообщений об ошибках
 const jwt = require("jsonwebtoken"); //для работы с jwt
 const { secret } = require("../config"); // получаем секретный ключ
-
+const { v1 } = require("uuid");
 
 //создаем функцию которая принимает ИД и роль и засовываем эт овсе в обьект пайлоад
 const generateAccessToken = (id, roles) => {
-  const payload = {
-    id,
-    roles,
-  };
-  return jwt.sign(payload, secret, { expiresIn: "24h" }); //передаем обьект, секретный ключ который храниться на сервере и опции
+    const payload = {
+        id,
+        roles,
+    };
+    return jwt.sign(payload, secret, { expiresIn: "24h" }); //передаем обьект, секретный ключ который храниться на сервере и опции
 };
 
 class authController {
-  async registration(req, res) {
-    try {
-      //валидация
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res
-          .status(400)
-          .json({ message: errors.errors[0].msg, errors });
-      }
+    async registration(req, res) {
+        try {
+            //валидация
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ message: errors.errors[0].msg, errors });
+            }
 
-      const { username, email, password } = req.body;
-      const candidate = await User.findOne({ username }); //ищем пользователя с этим юзернеймом
-      if (candidate) {
-        return res.status(400).json({ message: "Пользователь уже существует" });
-      }
+            const { email, password } = req.body;
+            const candidate = await User.findOne({ email }); //ищем пользователя с этим email
+            if (candidate) {
+                return res.status(400).json({ message: "Пользователь уже существует" });
+            }
 
-      var hashPassword = bcrypt.hashSync(password, 7); //Хэшируем пароль
+            var hashPassword = bcrypt.hashSync(password, 7); //Хэшируем пароль
 
-      const userRole = await Role.findOne({ value: "User" }); //присваиваем роль Юзера
+            const userRole = await Role.findOne({ value: "User" }); //присваиваем роль Юзера
 
-      const user = new User({
-        username,
-        password: hashPassword,
-        email,
-        roles: [userRole.value],
-      }); //создаем пользователя
+            const user = new User({
+                user_id: v1(),
+                password: hashPassword,
+                email,
+                roles: [userRole.value],
+            }); //создаем пользователя
 
-      await user.save(); //сохраняем в БД
+            await user.save(); //сохраняем в БД
 
-      return res.json({ message: "Пользователь зарегестрирован" }); //мессадж
-    } catch (e) {
-      console.log(e);
-      res.status(400).json({ message: "Registration Error" });
+            return res.json({ message: "Пользователь зарегестрирован" }); //мессадж
+        } catch (e) {
+            console.log(e);
+            res.status(400).json({ message: "Registration Error" });
+        }
     }
-  }
 
+    async login(req, res) {
+        try {
+            const { email, password } = req.body; //получаем данные
 
+            const user = await User.findOne({ email }); // ищем юзера
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
 
-  async login(req, res) {
-    try {
-      const { username, password } = req.body; //получаем данные
+            const validPassword = bcrypt.compareSync(password, user.password); //валидация введенного пароля
+            if (!validPassword) {
+                return res.status(400).json({ message: "Password incorrect" });
+            }
 
-      const user = await User.findOne({ username }); // ищем юзера
-      if (!user) {
-       return res.status(400).json({ message: "User not found" });
-      }
-
-      const validPassword = bcrypt.compareSync(password, user.password); //валидация введенного пароля
-      if (!validPassword) {
-        return res.status(400).json({ message: "Password incorrect" });
-      }
-      
-
-      const token = generateAccessToken(user._id, user.roles); // _id монго генерирует сам
-      return res.json({token, email: user.email, roles: user.roles, username: user.username})
-
-    } catch (e) {
-      console.log(e);
-      res.status(400).json({ message: "Login Error" });
+            const token = generateAccessToken(user._id, user.roles); // _id монго генерирует сам
+            return res.json({ token, email: user.email, roles: user.roles, user_id: user.user_id });
+        } catch (e) {
+            console.log(e);
+            res.status(400).json({ message: "Login Error" });
+        }
     }
-  }
 
-  async me(req, res) {
-    try {
-      const { user } = req.params;
-  
-      const userData = await User.findOne({ username: user })
-      return res.status(200).json({username: userData.username, role: userData.roles[0]})
+    async me(req, res) {
+        try {
+            const { user_id } = req.params;
 
-    } catch (e) {
-      return res.status(403).json({ message: "You are not autorized" });
+            const userData = await User.findOne({ user_id });
+            return res.status(200).json({ email: userData.email, role: userData.roles[0], user_id: userData.user_id });
+        } catch (e) {
+            return res.status(403).json({ message: "You are not autorized" });
+        }
     }
-  }
 
-  async getUsers(req, res) {
-    try {
-      //todo создаем роли(один раз)
-      // const userRole = new Role();
-      // const adminRole = new Role({value: "ADMIN"})
-      // await userRole.save();
-      // await adminRole.save()
+    async getUsers(req, res) {
+        try {
+            //todo создаем роли(один раз)
+            // const userRole = new Role();
+            // const adminRole = new Role({value: "ADMIN"})
+            // await userRole.save();
+            // await adminRole.save()
 
-      const users = await User.find(); //получаем юзеров
-      res.json(users)
-
-    } catch (e) {
-      console.log(e);
+            const users = await User.find(); //получаем юзеров
+            res.json(users);
+        } catch (e) {
+            console.log(e);
+        }
     }
-  }
 }
 
 module.exports = new authController();
--- auth/authRouter.js
+++ auth/authRouter.js
@@ -2,18 +2,17 @@ const Router = require("express");
 const router = new Router();
 const controller = require("./authController");
 const { check } = require("express-validator"); //валидатор
-const authMiddleware = require('./middleware/authMiddleware');
-const roleMiddleware = require('./middleware/roleMiddleware');
+const authMiddleware = require("./middleware/authMiddleware");
+const roleMiddleware = require("./middleware/roleMiddleware");
 
 router.post(
-  "/registration",
-  [check("username", "Имя пользователя не может быть пустым").notEmpty(), //todo вторым параметром валидация, первый параметр это проверяемое поле, второе это сообщение
-check("password", " Пароль должен быть больше 4 и меньше 10 символов").isLength({min: 4, max: 10})],
-  controller.registration
+    "/registration",
+    [check("email", "Email format is incorrect").notEmpty().isEmail(), check("password", " Password must be more than 4 and less than 10 symbols").isLength({ min: 4, max: 10 })],
+    controller.registration
 );
 router.post("/login", controller.login);
-router.get("/me/:user", authMiddleware, controller.me);
+router.get("/me/:user_id", authMiddleware, controller.me);
 // router.get("/users", authMiddleware,  controller.getUsers); //todo добавляем мидлварю чтоб только зареганый юзер мог сделать такой запрос
-router.get("/users", roleMiddleware(['ADMIN', 'User']),  controller.getUsers); //todo добавляем мидлварю чтоб только юзер с указанной ролью мог сделать такой запрос
+router.get("/users", roleMiddleware(["ADMIN", "User"]), controller.getUsers); //todo добавляем мидлварю чтоб только юзер с указанной ролью мог сделать такой запрос
 
 module.exports = router;
